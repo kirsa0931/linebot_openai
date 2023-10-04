c0043b4f73ed5e434c3322a32415e124211ae4c5
kirsa0931 <146957672+kirsa0931@users.noreply.github.com>
2023-10-04 23:09:50 +0800

    Update app.py

--- app.py
+++ app.py
@@ -27,17 +27,10 @@ openai.api_key = os.getenv('OPENAI_API_KEY')
 
 def GPT_response(text):
     # 接收回應
-    response = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
-    messages=[
-        {"role": "system", "content": "You are a helpful assistant."},
-        {"role": "user", "content": background_knowledge},  # 添加背景知识
-        {"role": "user", "content": conversation_data}
-     ]
-   )
+    response = openai.Completion.create(model="text-davinci-003", prompt=text, temperature=0.5, max_tokens=500)
     print(response)
     # 重組回應
-    answer = response['choices'][0]['message']['content'].strip()
+    answer = response['choices'][0]['text'].replace('。','')
     return answer
 
 
