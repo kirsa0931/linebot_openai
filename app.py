e374fa06e84ddd1d428a6872e88ab575b7d1b14c
kirsa0931 <146957672+kirsa0931@users.noreply.github.com>
2023-11-17 22:23:19 +0800

    Update app.py

--- app.py
+++ app.py
@@ -26,6 +26,7 @@ openai.api_key = os.getenv('OPENAI_API_KEY')
 
 file1_path = '狼人殺data.txt'
 file2_path = '對局data.txt'
+file3_path = '對局微調.txt'
 
 # 用读取模式打开文件
 with open(file1_path, 'r', encoding='utf-8') as file1:
@@ -34,75 +35,8 @@ with open(file1_path, 'r', encoding='utf-8') as file1:
 with open(file2_path, 'r', encoding='utf-8') as file2:
    Game_iformation  = file2.read()
     
-# 添加一个用于记录发言的文件路径
-#recorded_messages_file = 'recorded_messages.txt'
-
-def GPT_response(text):
-    # 接收回應
-    response = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
-    messages=[
-        {"role": "system", "content": "You are a helpful assistant"},
-        {"role": "user", "content": background_knowledge},
-        {"role": "user", "content": Game_iformation},
-        {"role": "user", "content": text}
-    ]
-)
-
-
-# 監聽所有來自 /callback 的 Post Request
-@app.route("/callback", methods=['POST'])
-def callback():
-    # get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature']
-    # get request body as text
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
-    # handle webhook body
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
-    return 'OK'
-
-
-
-from flask import Flask, request, abort
-
-from linebot import (
-    LineBotApi, WebhookHandler
-)
-from linebot.exceptions import (
-    InvalidSignatureError
-)
-from linebot.models import *
-
-#======python的函數庫==========
-import tempfile, os
-import datetime
-import openai
-import time
-#======python的函數庫==========
-
-app = Flask(__name__)
-static_tmp_path = os.path.join(os.path.dirname(__file__), 'static', 'tmp')
-# Channel Access Token
-line_bot_api = LineBotApi(os.getenv('CHANNEL_ACCESS_TOKEN'))
-# Channel Secret
-handler = WebhookHandler(os.getenv('CHANNEL_SECRET'))
-# OPENAI API Key初始化設定
-openai.api_key = os.getenv('OPENAI_API_KEY')
-
-file1_path = '狼人殺data.txt'
-file2_path = '對局data.txt'
-
-# 用读取模式打开文件
-with open(file1_path, 'r', encoding='utf-8') as file1:
-    background_knowledge = file1.read()
-
-with open(file2_path, 'r', encoding='utf-8') as file2:
-   Game_iformation  = file2.read()
-
+with open(file3_path, 'r', encoding='utf-8') as file3:
+   fine_tuning_data  = file3.read()
 
 def GPT_response(text):
     # 接收回應
