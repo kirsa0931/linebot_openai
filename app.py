b8b7c21818bb6d0f8ef60db3ed56b0dd720cba67
kirsa0931 <146957672+kirsa0931@users.noreply.github.com>
2023-11-18 13:35:02 +0800

    Update app.py

--- app.py
+++ app.py
@@ -83,7 +83,8 @@ def handle_message(event):
         print(GPT_answer)
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text=GPT_answer))
     except:
-        line_bot_api.reply_message(event.reply_token, TextSendMessage('你所使用的OPENAI API key額度可能已經超過，請於後台Log內確認錯誤訊息'))
+        print("error")
+        #line_bot_api.reply_message(event.reply_token, TextSendMessage('你所使用的OPENAI API key額度可能已經超過，請於後台Log內確認錯誤訊息'))
         
 
 @handler.add(PostbackEvent)
