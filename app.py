506597d473f9930d94f1f10e7fd2a4733ae6d33e
kirsa0931 <146957672+kirsa0931@users.noreply.github.com>
2023-12-08 15:32:04 +0800

    Update app.py

--- app.py
+++ app.py
@@ -126,5 +126,4 @@ def welcome(event):
 import os
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
-    app.config['TIMEOUT'] = 360000
     app.run(host='0.0.0.0', port=port)
