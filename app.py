15610ef6723680e79536941332dea5bb7896f53e
kirsa0931 <146957672+kirsa0931@users.noreply.github.com>
2023-12-08 15:57:14 +0800

    Update app.py

--- app.py
+++ app.py
@@ -55,6 +55,7 @@ def GPT_response(text):
             {"role": "system", "content": "You are a player in the game."},
             {"role": "user", "content": background_knowledge},
             {"role": "user", "content": fine_tuning_data},
+            {"role": "user", "content": prompt_set},
             {"role": "user", "content": Game_iform},
             {"role": "user", "content": text}
         ]
@@ -90,7 +91,6 @@ def handle_message(event):
             messages=[
                 {"role": "system", "content": "你是一個狼人殺玩家"},
                 {"role": "user", "content": rule},
-                {"role": "user", "content": prompt_set},
                 {"role": "user", "content": recorded_messages},
             ]
         )
